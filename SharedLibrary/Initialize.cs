d21a61be556e37a91a3fd3a2bc5fc29d6a4074ee
Андрей <flitenym@gmail.com>
2020-04-13 20:22:04 +0500

    app in library

---
+++ SharedLibrary/Initialize.cs
@@ -0,0 +1,86 @@
+﻿using SharedLibrary.AbstractClasses;
+using SharedLibrary.Helper;
+using SharedLibrary.Helper.StaticInfo;
+using SharedLibrary.Provider;
+using SharedLibrary.View;
+using SharedLibrary.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace SharedLibrary
+{
+    public static class Initialize
+    {
+        public static void OnDispatcherUnhandledException(object sender, System.Windows.Threading.DispatcherUnhandledExceptionEventArgs e)
+        {
+            Task.Factory.StartNew(() => HelperMethods.Message($"Глобальная ошибка: {e.Exception.Message}"));
+
+            var infoVM = SharedProvider.GetFromDictionaryByKey(nameof(InfoViewModel)) as InfoViewModel ?? new InfoViewModel();
+            infoVM.UpdateStackTrace(e.Exception.StackTrace);
+            e.Handled = true;
+        }
+
+        public static void Start(object sender, StartupEventArgs e)
+        {
+            Application.Current.Dispatcher.UnhandledException += OnDispatcherUnhandledException;
+
+            var splashScreen = new SplashScreenWindowView();
+            Application.Current.MainWindow = splashScreen;
+            splashScreen.Show();
+
+            Task.Factory.StartNew(() =>
+            {
+                Application.Current.Dispatcher.Invoke(() =>
+                {
+                    //Все вьюшки
+                    SharedProvider.SetToSingleton(
+                        InfoKeys.ModulesKey,
+                        HelperMethods.GetAllInstancesOf<ModuleBase>(new List<ModuleBase>()).Where(x => x.IsActive).ToList());
+
+                    //Все темы
+                    SharedProvider.SetToSingleton(
+                        InfoKeys.ThemesKey,
+                        HelperMethods.GetAllInstancesOf<ThemeBase>(new List<ThemeBase>()).ToList());
+
+                    //Основная ViewModel
+                    var vm = new MainWindowViewModel();
+                    SharedProvider.SetToSingleton(nameof(MainWindowViewModel), vm);
+
+                    var infovm = new InfoViewModel();
+                    SharedProvider.SetToSingleton(nameof(InfoViewModel), infovm);
+
+                    var mainWindow = new MainWindowView();
+
+                    mainWindow.DataContext = vm;
+                    Application.Current.MainWindow = mainWindow;
+                    mainWindow.Show();
+                    splashScreen.Close();
+                    mainWindow.Closing += async (s, args) =>
+                    {
+                        try
+                        {
+                            if (vm.SelectedTheme != null)
+                                vm.SelectedTheme.Deactivate();
+
+                            if (vm.SelectedThemeDarkOrLight != null)
+                                vm.SelectedThemeDarkOrLight.Deactivate();
+
+                            if (vm.SelectedViewModel != null)
+                                vm.SelectedViewModel.ModuleBaseItem.Deactivate();
+
+                            Application.Current.Dispatcher.UnhandledException -= OnDispatcherUnhandledException;
+                        }
+                        catch (Exception ex)
+                        {
+                            await HelperMethods.Message($"{ex.Message}");
+                        }
+                    };
+                });
+            });
+        }
+    }
+}
--- Van/App.xaml.cs
+++ Van/App.xaml.cs
@@ -1,15 +1,5 @@
-﻿using System;
-using System.Linq;
-using System.Threading.Tasks;
-using System.Windows;
-using SharedLibrary.AbstractClasses;
-using SharedLibrary.Helper;
-using SharedLibrary.Helper.StaticInfo;
-using SharedLibrary.ViewModel;
-using SharedLibrary.Provider;
-using SharedLibrary.View;
-using static SharedLibrary.Helper.HelperMethods;
-using System.Collections.Generic;
+﻿using System.Windows; 
+using SharedLibrary;
 
 namespace Van
 {
@@ -18,75 +8,9 @@ namespace Van
     /// </summary>
     public partial class App : Application
     {
-        public App()
-        {
-            this.Dispatcher.UnhandledException += OnDispatcherUnhandledException;
-        }
-
-        private void OnDispatcherUnhandledException(object sender, System.Windows.Threading.DispatcherUnhandledExceptionEventArgs e)
-        {
-            Task.Factory.StartNew(() => HelperMethods.Message($"Глобальная ошибка: {e.Exception.Message}"));
-
-            var infoVM = SharedProvider.GetFromDictionaryByKey(nameof(InfoViewModel)) as InfoViewModel ?? new InfoViewModel();
-            infoVM.UpdateStackTrace(e.Exception.StackTrace);
-            e.Handled = true;
-        }
-
         private void Application_Startup(object sender, StartupEventArgs e)
         {
-            var splashScreen = new SplashScreenWindowView();
-            this.MainWindow = splashScreen;
-            splashScreen.Show();
-
-            Task.Factory.StartNew(() =>
-            {
-                this.Dispatcher.Invoke(() =>
-                {
-                    //Все вьюшки
-                    SharedProvider.SetToSingleton(
-                        InfoKeys.ModulesKey,
-                        HelperMethods.GetAllInstancesOf<ModuleBase>(new List<ModuleBase>()).Where(x => x.IsActive).ToList());
-
-                    //Все темы
-                    SharedProvider.SetToSingleton(
-                        InfoKeys.ThemesKey,
-                        HelperMethods.GetAllInstancesOf<ThemeBase>(new List<ThemeBase>()).ToList());
-
-                    //Основная ViewModel
-                    var vm = new MainWindowViewModel();
-                    SharedProvider.SetToSingleton(nameof(MainWindowViewModel), vm);
-
-                    var infovm = new InfoViewModel();
-                    SharedProvider.SetToSingleton(nameof(InfoViewModel), infovm);
-
-                    var mainWindow = new MainWindowView();
-
-                    mainWindow.DataContext = vm;
-                    this.MainWindow = mainWindow;
-                    mainWindow.Show();
-                    splashScreen.Close();
-                    mainWindow.Closing += async (s, args) =>
-                    {
-                        try
-                        {
-                            if (vm.SelectedTheme != null)
-                                vm.SelectedTheme.Deactivate();
-
-                            if (vm.SelectedThemeDarkOrLight != null)
-                                vm.SelectedThemeDarkOrLight.Deactivate();
-
-                            if (vm.SelectedViewModel != null)
-                                vm.SelectedViewModel.ModuleBaseItem.Deactivate();
-
-                            this.Dispatcher.UnhandledException -= OnDispatcherUnhandledException;
-                        }
-                        catch (Exception ex)
-                        {
-                            await Message($"{ex.Message}");
-                        }
-                    };
-                });
-            });
+            Initialize.Start(sender, e);
         }
     }
 }
